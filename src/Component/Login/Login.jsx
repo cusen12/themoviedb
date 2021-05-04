39fa92546515c8d1e6a7b24515a585f36aaf0e45
Tuan 2 Nguyen Thanh <v3210106@transcosmos-vn.local>
2021-05-04 16:00:34 +0700

    login full width md

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -90,7 +90,7 @@ function Login() {
     return (
         <Grid container item className="login" md={12}>
             {loginData !== undefined ? 
-                <Grid md={12}>
+                <Grid item md={12}>
                     {
                        loginData.success===true ? 
                        <Grid container  
