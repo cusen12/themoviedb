3dad91ab135d652b9b13cfce371333c2c7303e08
Tuân Nguyễn <tuannt1930@gmail.com>
2021-05-29 23:54:33 +0700

    asdasds

--- src/Component/ChartSVG/ChartSVG.jsx
+++ src/Component/ChartSVG/ChartSVG.jsx
@@ -1,4 +1,4 @@
-import { Box, CircularProgress, Grid, Typography } from '@material-ui/core';
+import { Box, Grid, Typography } from '@material-ui/core';
 import React from 'react'; 
 
 function ChartSVG(props) { 
