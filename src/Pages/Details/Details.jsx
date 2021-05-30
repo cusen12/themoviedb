2602379bf67ab365537a4234fdaf33a6a1b8c11e
Tuân Nguyễn <tuannt1930@gmail.com>
2021-05-30 23:05:28 +0700

    asad

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -204,8 +204,8 @@ function Details() {
    
        
     const sectionData = useSelector(state=>state.section.value);   
-    const sectionId = sectionData.session_id
-    const checkLogin = useSelector(state => state.login.value.success)
+    const sectionId = sectionData?.session_id
+    const checkLogin = useSelector(state => state.login.value?.success)
     const idUser = useSelector(state=>state.getIDLogin.value)
     const handleAddtoWatchlist = () => {    
         const addtoWatchList = async () =>{ 
--- src/Pages/DetailsTV/DetailsTV.jsx
+++ src/Pages/DetailsTV/DetailsTV.jsx
@@ -162,8 +162,8 @@ function DetailsTV() {
    
        
     const sectionData = useSelector(state=>state.section.value);   
-    const sectionId = sectionData.session_id
-    const checkLogin = useSelector(state => state.login.value.success)
+    const sectionId = sectionData?.session_id
+    const checkLogin = useSelector(state => state.login.value?.success)
     const idUser = useSelector(state=>state.getIDLogin.value)
     const handleAddtoWatchlist = () => {    
         const addtoWatchList = async () =>{ 
